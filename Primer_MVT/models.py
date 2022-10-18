3171f8c3b35c2b491333256ab76a461e9e803804
LeonardoDepetris <ldepetris@coopmorteros.coop>
2022-10-18 11:54:20 -0300

    Se agregó fecha de naciemiento

--- Primer_MVT/models.py
+++ Primer_MVT/models.py
@@ -2,9 +2,10 @@
 from django.db import models
 
 class Familiar(models.Model):
-    nombre = models.CharField(max_length=100)
-    direccion = models.CharField(max_length=200)
+    nombre = models.CharField(max_length=100,default = "")
+    direccion = models.CharField(max_length=200,default = "")
     numero_pasaporte = models.IntegerField()
+    fecha_nacimiento = models.CharField(max_length=20,default = "")
 
 def __str__(self):
-    return f"{self.nombre}, {self.numero_pasaporte}, {self.id}"
+    return f"{self.nombre}, {self.numero_pasaporte}, {self.fecha_nacimiento}, {self.id}"
