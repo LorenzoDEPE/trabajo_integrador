60922b96cae008015ef2f5b59bf172739db6f82f
Lorenzo <depetrislorenzo1@gmail.com>
2022-10-18 09:21:17 -0300

    primer app django

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Primer_MVT.apps.PrimerMvtConfig', # ESTA ES LA NUEVA APP !!
 ]
 
 MIDDLEWARE = [
--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Primer_MVT.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludar/', index), # ESTA ES LA NUEVA FUNCTION
 ]
