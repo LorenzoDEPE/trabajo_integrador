ea0389bf2f8afc34b05397f41fdd043889b5c21d
Lorenzo <depetrislorenzo1@gmail.com>
2022-10-27 20:51:18 -0300

    agregado de posteos

--- Primer_MVT/forms.py
+++ Primer_MVT/forms.py
@@ -1,11 +1,22 @@
 from django import forms
 from Primer_MVT.models import Familiar
+from Primer_MVT.models import Posteo
+from Primer_MVT.models import Categoria
 
 class FamiliarForm(forms.ModelForm):
   class Meta:
     model = Familiar
     fields = ['nombre', 'direccion', 'numero_pasaporte', 'fecha_nacimiento']
-    
+
+class PosteoForm(forms.ModelForm):
+  class Meta:
+    model = Posteo
+    fields = ['mensaje', 'titulo', 'autor', 'fecha']
+
+class CategoriaForm(forms.ModelForm):
+  class Meta:
+    model = Categoria
+    fields = ['nombre', 'descripcion']
     
 class Buscar(forms.Form):
       nombre = forms.CharField(max_length=100)
